8acfbfb281a7ce75b521062818c3b86ec4aec3cd
Jayanta Biswas <bjayanta.me@gmail.com>
2017-07-30 15:51:29 +0600

    send json data into server using post method

--- app/src/main/java/com/freelanceitlab/walletprototype/SignupFragment.java
+++ app/src/main/java/com/freelanceitlab/walletprototype/SignupFragment.java
@@ -11,19 +11,34 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class SignupFragment extends Fragment {
+
+    EditText name;
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText referral_email;
+    Spinner country;
+
+    String dataObject;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,8 +46,8 @@ public class SignupFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
 
-        TextView textView = (TextView) view.findViewById(R.id.back);
-        textView.setOnClickListener(new View.OnClickListener() {
+        Button backBtn = (Button) view.findViewById(R.id.back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getFragmentManager();
@@ -43,10 +58,18 @@ public class SignupFragment extends Fragment {
             }
         });
 
+        name = (EditText) view.findViewById(R.id.name);
+        username = (EditText) view.findViewById(R.id.username);
+        email = (EditText) view.findViewById(R.id.email);
+        password = (EditText) view.findViewById(R.id.password);
+        referral_email = (EditText) view.findViewById(R.id.referral_email);
+        country = (Spinner) view.findViewById(R.id.country);
+
         Button signup = (Button) view.findViewById(R.id.signup);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dataObject = formatDataAsJson();
                 sendDataToServer();
             }
         });
@@ -58,8 +81,12 @@ public class SignupFragment extends Fragment {
         final JSONObject root = new JSONObject();
 
         try {
-            root.put("name", "Jayanta Biswas");
-            root.put("email", "bjayanta.neo@gmail.com");
+            root.put("name", name.getText());
+            root.put("username", username.getText());
+            root.put("email", email.getText());
+            root.put("password", password.getText());
+            root.put("referral_email", referral_email.getText());
+            root.put("country", country.getSelectedItem().toString());
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -68,12 +95,12 @@ public class SignupFragment extends Fragment {
     }
 
     private void sendDataToServer() {
-        final String data = formatDataAsJson();
+        // final String data = formatDataAsJson();
 
         new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
-                return getServerResponse(data);
+                return getServerResponse();
             }
 
             @Override
@@ -87,9 +114,10 @@ public class SignupFragment extends Fragment {
 
     }
 
-    private String getServerResponse(String data) {
+    private String getServerResponse() {
         String key = "db5d8d6959ccb6288afffa1b018631f5";
         String userdata = getUserdata();
+        String output = "";
 
         try {
             JSONObject object = new JSONObject(userdata);
@@ -98,6 +126,7 @@ public class SignupFragment extends Fragment {
             String password = object.getString("password");
 
             URL url = new URL("http://dbsewallet.com/api/permission/save?key=" + key + "&user=" + username + "&pass=" + password);
+            String urlParameters = "details="  + URLEncoder.encode(dataObject.toString(), "UTF-8");
 
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -105,7 +134,27 @@ public class SignupFragment extends Fragment {
             connection.setRequestProperty("USER-AGENT", "Android");
             connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
 
+            connection.setDoOutput(true);
+            DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+            dStream.writeBytes(urlParameters);
+            dStream.flush();
+            dStream.close();
 
+            int responseCode = connection.getResponseCode();
+            output += "Response URL " + url;
+            output += System.getProperty("line.separator") + "Request Parameters " + urlParameters;
+            output += System.getProperty("line.separator") + "Response Code " + responseCode;
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = "";
+            StringBuilder responseOutput = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                responseOutput.append(line);
+            }
+            br.close();
+
+            output += System.getProperty("line.separator") + responseOutput.toString();
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -115,7 +164,7 @@ public class SignupFragment extends Fragment {
             e.printStackTrace();
         }
 
-        return null;
+        return output;
     }
 
     private String getUserdata() {
